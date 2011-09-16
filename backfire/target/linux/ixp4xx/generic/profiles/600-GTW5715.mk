ab5712b80f14f7eca38b5b2ae98edf532fae064c
lintel.huang@gmail.com <lintel.huang@gmail.com@74e81aba-36f0-4891-8cfb-1815f9025a0c>
2011-09-16 04:48:14 +0000

    添加 WRV54G 的配置文件

---
+++ backfire/target/linux/ixp4xx/generic/profiles/600-GTW5715.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2006 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/GTWX5715
+  NAME:=GTWX5715(WRV54G)
+  PACKAGES:=kmod-via-velocity \
+	kmod-ath5k wireless-tools \
+	kmod-scsi-core \
+	kmod-ata-core kmod-ata-artop \
+	kmod-usb-core kmod-usb-uhci kmod-usb2 kmod-usb-storage \
+	kmod-fs-ext2 kmod-fs-ext3 kmod-ath9k
+endef
+
+define Profile/GTWX5715/Description
+	Package set optimized for the Linksys WRV54G
+endef
+$(eval $(call Profile,GTWX5715))
+
