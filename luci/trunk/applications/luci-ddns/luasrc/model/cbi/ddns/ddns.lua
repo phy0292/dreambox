32b1f027a3747bef4b1ced1d5fbd4fe4a8edf0d5
flyzjhz@gmail.com <flyzjhz@gmail.com@74e81aba-36f0-4891-8cfb-1815f9025a0c>
2011-10-04 08:06:53 +0000

    变更ddns设置页面

--- luci/trunk/applications/luci-ddns/luasrc/model/cbi/ddns/ddns.lua
+++ luci/trunk/applications/luci-ddns/luasrc/model/cbi/ddns/ddns.lua
@@ -13,13 +13,21 @@ You may obtain a copy of the License at
 $Id: ddns.lua 6588 2010-11-29 15:14:50Z jow $
 ]]--
 
-local is_mini = (luci.dispatcher.context.path[1] == "mini")
-
+require("luci.tools.webadmin")
 
 m = Map("ddns", translate("Dynamic DNS"),
-	translate("Dynamic DNS allows that your router can be reached with " ..
-		"a fixed hostname while having a dynamically changing " ..
-		"IP address."))
+	translate("Dynamic DNS allows that your router can be reached with a fixed hostname while having a dynamically changing IP address."))
+
+s = m:section(TypedSection, "goble", "")
+s.addremove = false
+s.anonymous = true
+
+
+s:option(Flag, "enabled", translate("Enable"))
+
+s:option(Value, "check_interval",	translate("Check for changed IP every"),translate("interval minute")).default = 10
+
+
 
 s = m:section(TypedSection, "service", "")
 s.addremove = true
@@ -66,59 +74,39 @@ end
 
 svc:value("-", "-- "..translate("custom").." --")
 
+
 url = s:option(Value, "update_url", translate("Custom update-URL"))
 url:depends("service_name", "-")
 url.rmempty = true
 
+neiwang = s:option(Flag, "neiwang", translate("Intranet"),translate("Secondary route setting"))
+neiwang:depends("service_name", "3322.org")
+neiwang:depends("service_name", "dyndns.org")
+
+neiwang.disabled = 0
+
+
+
 s:option(Value, "domain", translate("Hostname")).rmempty = true
+
 s:option(Value, "username", translate("Username")).rmempty = true
+
 pw = s:option(Value, "password", translate("Password"))
 pw.rmempty = true
 pw.password = true
+	
+iface = s:option(ListValue, "ip_network", translate("Network"))
+iface.rmempty = true
+luci.tools.webadmin.cbi_add_networks(iface)
 
+uptime = s:option(DummyValue, "uptime", translate("ddns update time","ddns update time"))
+uptime.optional = false
+uptime.rmempty = true
 
-if is_mini then
-	s.defaults.ip_source = "network"
-	s.defaults.ip_network = "wan"
-else
-	require("luci.tools.webadmin")
+ipaddr = s:option(DummyValue, "ipaddr", translate("ip address","ip address"))
+ipaddr.optional = false
+ipaddr.rmempty = true
 
-	src = s:option(ListValue, "ip_source",
-		translate("Source of IP address"))
-	src:value("network", translate("network"))
-	src:value("interface", translate("interface"))
-	src:value("web", translate("URL"))
-
-	iface = s:option(ListValue, "ip_network", translate("Network"))
-	iface:depends("ip_source", "network")
-	iface.rmempty = true
-	luci.tools.webadmin.cbi_add_networks(iface)
-
-	iface = s:option(ListValue, "ip_interface", translate("Interface"))
-	iface:depends("ip_source", "interface")
-	iface.rmempty = true
-	for k, v in pairs(luci.sys.net.devices()) do
-		iface:value(v)
-	end
-
-	web = s:option(Value, "ip_url", translate("URL"))
-	web:depends("ip_source", "web")
-	web.rmempty = true
-end
-
-
-s:option(Value, "check_interval",
-	translate("Check for changed IP every")).default = 10
-unit = s:option(ListValue, "check_unit", translate("Check-time unit"))
-unit.default = "minutes"
-unit:value("minutes", translate("min"))
-unit:value("hours", translate("h"))
-
-s:option(Value, "force_interval", translate("Force update every")).default = 72
-unit = s:option(ListValue, "force_unit", translate("Force-time unit"))
-unit.default = "hours"
-unit:value("minutes", translate("min"))
-unit:value("hours", translate("h"))
-
+local lucierror = luci.sys.lucierror()
 
 return m
