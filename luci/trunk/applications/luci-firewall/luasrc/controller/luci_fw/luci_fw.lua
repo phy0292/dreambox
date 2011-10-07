a66b0f70277b68e4c4ea3e6f6cea3c8497438f23
flyzjhz@gmail.com <flyzjhz@gmail.com@74e81aba-36f0-4891-8cfb-1815f9025a0c>
2011-10-07 04:46:28 +0000

    重新调整端口映射，调用系统原有的端口映射设定

--- luci/trunk/applications/luci-firewall/luasrc/controller/luci_fw/luci_fw.lua
+++ luci/trunk/applications/luci-firewall/luasrc/controller/luci_fw/luci_fw.lua
@@ -1,11 +1,10 @@
 module("luci.controller.luci_fw.luci_fw", package.seeall)
 
 function index()
-	entry({"admin", "network", "firewall"}, alias("admin", "network", "firewall", "zones"), _("Firewall"), 60).i18n = "firewall"
+	entry({"admin", "network", "firewall"}, alias("admin", "network", "firewall", "zones"), _("Firewall"), 28).i18n = "firewall"
 	entry({"admin", "network", "firewall", "zones"}, arcombine(cbi("luci_fw/zones"), cbi("luci_fw/zone")), nil, 10).leaf = true
 	entry({"admin", "network", "firewall", "rule"}, arcombine(cbi("luci_fw/zones"), cbi("luci_fw/trule")), nil, 20).leaf = true
-	entry({"admin", "network", "firewall", "portmap"}, arcombine(cbi("luci_fw/zones"), cbi("luci_fw/portmap")), nil, 25).leaf = true
 	entry({"admin", "network", "firewall", "redirect"}, arcombine(cbi("luci_fw/zones"), cbi("luci_fw/rrule")), nil, 30).leaf = true
-	entry({"mini", "network", "portfw"}, cbi("luci_fw/miniportfw", {autoapply=true}), _("Port forwarding"), 70).i18n = "firewall"
+	entry({"admin", "network", "portfw"}, cbi("luci_fw/miniportfw"), _("portmap"), 29).i18n = "firewall"
 end
 
