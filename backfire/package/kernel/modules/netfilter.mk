3d73e9353329379090231f04493509c0ec32d8f6
flyzjhz@gmail.com <flyzjhz@gmail.com@74e81aba-36f0-4891-8cfb-1815f9025a0c>
2011-10-08 07:23:10 +0000

    weburl， webmon， bandwidth， timerange   四个模块，独立打包编译

--- backfire/package/kernel/modules/netfilter.mk
+++ backfire/package/kernel/modules/netfilter.mk
@@ -26,15 +26,76 @@ define KernelPackage/ipt-core/description
  - LOG
  - mac
  - multiport
- - webmon
- - weburl
- - bandwidth
- - timerange
  - REJECT
  - TCPMSS
 endef
 
 $(eval $(call KernelPackage,ipt-core))
+
+
+define KernelPackage/ipt-webmon
+  SUBMENU:=$(NF_MENU)
+  TITLE:=Netfilter webmon
+  KCONFIG:=$(KCONFIG_IPT_WEBMON)
+  FILES:=$(foreach mod,$(IPT_WEBMON-m),$(LINUX_DIR)/net/$(mod).$(LINUX_KMOD_SUFFIX))
+  AUTOLOAD:=$(call AutoLoad,40,$(notdir $(IPT_WEBMON-m)))
+endef
+
+define KernelPackage/ipt-webmon/description
+ Netfilter webmon kernel modules
+ Includes:
+ - webmon
+endef
+
+$(eval $(call KernelPackage,ipt-webmon))
+
+define KernelPackage/ipt-weburl
+  SUBMENU:=$(NF_MENU)
+  TITLE:=Netfilter weburl
+  KCONFIG:=$(KCONFIG_IPT_WEBURL)
+  FILES:=$(foreach mod,$(IPT_WEBURL-m),$(LINUX_DIR)/net/$(mod).$(LINUX_KMOD_SUFFIX))
+  AUTOLOAD:=$(call AutoLoad,40,$(notdir $(IPT_WEBURL-m)))
+endef
+
+define KernelPackage/ipt-weburl/description
+ Netfilter core kernel modules
+ Includes:
+ - weburl
+endef
+
+$(eval $(call KernelPackage,ipt-weburl))
+
+define KernelPackage/ipt-bandwidth
+  SUBMENU:=$(NF_MENU)
+  TITLE:=Netfilter bandwidth
+  KCONFIG:=$(KCONFIG_IPT_BANDWIDTH)
+  FILES:=$(foreach mod,$(IPT_BANDWIDTH-m),$(LINUX_DIR)/net/$(mod).$(LINUX_KMOD_SUFFIX))
+  AUTOLOAD:=$(call AutoLoad,40,$(notdir $(IPT_BANDWIDTH-m)))
+endef
+
+define KernelPackage/ipt-bandwidth/description
+ Netfilter bandwidth kernel modules
+ Includes:
+ - bandwidth
+endef
+
+$(eval $(call KernelPackage,ipt-bandwidth))
+
+define KernelPackage/ipt-timerange
+  SUBMENU:=$(NF_MENU)
+  TITLE:=Netfilter timerange
+  KCONFIG:=$(KCONFIG_IPT_TIMERANGE)
+  FILES:=$(foreach mod,$(IPT_TIMERANGE-m),$(LINUX_DIR)/net/$(mod).$(LINUX_KMOD_SUFFIX))
+  AUTOLOAD:=$(call AutoLoad,40,$(notdir $(IPT_TIMERANGE-m)))
+endef
+
+define KernelPackage/ipt-core/description
+ Netfilter timerange kernel modules
+ Includes:
+ - timerange
+endef
+
+$(eval $(call KernelPackage,ipt-timerange))
 
 
 define KernelPackage/ipt/Depends
