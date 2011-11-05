5c73d527abb2b3e05300a357bf9855a4dbbfd521
flyzjhz@gmail.com <flyzjhz@gmail.com@74e81aba-36f0-4891-8cfb-1815f9025a0c>
2011-11-05 08:24:52 +0000

    修改计划任务设定页面

--- luci/trunk/modules/admin-full/luasrc/controller/admin/system.lua
+++ luci/trunk/modules/admin-full/luasrc/controller/admin/system.lua
@@ -28,7 +28,8 @@ function index()
 	end
 
 	entry({"admin", "system", "startup"}, form("admin_system/startup"), _("Startup"), 45)
-	entry({"admin", "system", "crontab"}, form("admin_system/crontab"), _("Scheduled Tasks"), 46)
+	entry({"admin", "system", "crontab"}, cbi("admin_system/crontab"), _("Scheduled Tasks"), 46)
+	entry({"admin", "system", "crontab","crontableaf"}, cbi("admin_system/crontableaf"), nil).leaf = true
 
 	--if nixio.fs.access("/etc/config/fstab") then
 	--	entry({"admin", "system", "fstab"}, cbi("admin_system/fstab"), _("Mount Points"), 50)
--- luci/trunk/modules/admin-full/luasrc/model/cbi/admin_system/crontab.lua
+++ luci/trunk/modules/admin-full/luasrc/model/cbi/admin_system/crontab.lua
@@ -1,8 +1,7 @@
 --[[
 LuCI - Lua Configuration Interface
 
-Copyright 2008 Steven Barth <steven@midlink.org>
-Copyright 2008 Jo-Philipp Wich <xm@leipzig.freifunk.net>
+Copyright 2011 flyzjhz <flyzjhz@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -10,28 +9,45 @@ You may obtain a copy of the License at
 
 	http://www.apache.org/licenses/LICENSE-2.0
 
-$Id$
+$Id: ddns.lua 6588 2010-11-29 15:14:50Z jow $
 ]]--
 
-local fs = require "nixio.fs"
-local cronfile = "/etc/crontabs/root" 
+require("luci.tools.webadmin")
 
-f = SimpleForm("crontab", translate("Scheduled Tasks"), translate("This is the system crontab in which scheduled tasks can be defined."))
+local fs   = require "nixio.fs"
+local util = require "nixio.util"
 
-t = f:field(TextValue, "crons")
-t.rmempty = true
-t.rows = 10
-function t.cfgvalue()
-	return fs.readfile(cronfile) or ""
-end
+m = Map("cron", translate("Scheduled Tasks"),
+	translate("This is the system crontab in which scheduled tasks can be defined."))
 
-function f.handle(self, state, data)
-	if state == FORM_VALID then
-		if data.crons then
-			fs.writefile(cronfile, data.crons:gsub("\r\n", "\n"))
-		end
+
+task = m:section(TypedSection, "task", "")
+task.anonymous = true
+task.addremove = true
+task.template = "cbi/tblsection"
+task.extedit  = luci.dispatcher.build_url("admin/system/crontab/crontableaf/%s")
+
+task.create = function(...)
+	local sid = TypedSection.create(...)
+	if sid then
+		luci.http.redirect(task.extedit % sid)
+		return
 	end
-	return true
 end
 
-return f
+
+enable=task:option(Flag, "enabled", translate("Enable"))
+enable.default = false
+enable.optional = false
+enable.rmempty = false
+enable.disabled = 0
+
+
+task_task_task=task:option(DummyValue, "task_name", translate("task name"))
+function task_task_task.cfgvalue(self, s)
+	return self.map:get(s, "task_name") or "none"
+end
+
+return m
+
+
