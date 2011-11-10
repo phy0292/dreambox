27d010e9ab3432cd654e6d09fa721c7ee9715029
flyzjhz@gmail.com <flyzjhz@gmail.com@74e81aba-36f0-4891-8cfb-1815f9025a0c>
2011-11-10 13:03:26 +0000

    ddns 页面增加 注册网址显示。

--- luci/trunk/applications/luci-ddns/luasrc/model/cbi/ddns/ddnsleaf.lua
+++ luci/trunk/applications/luci-ddns/luasrc/model/cbi/ddns/ddnsleaf.lua
@@ -76,6 +76,58 @@ url = s:option(Value, "update_url", translate("Custom update-URL"))
 url:depends("service_name", "-")
 url.rmempty = true
 
+weburl=s:option(Value, "update_url", translate("Custom update-URL"))
+
+weburl3322 = s:option( DummyValue,"weburl3322", translate("register url"),
+translate("<a target=\"_blank\" href='http://www.3322.org'>http://www.3322.org</a>"))
+weburl3322:depends("service_name", "3322.org")
+
+weburlchangip = s:option( DummyValue,"weburlchangip", translate("register url"),
+translate("<a target=\"_blank\" href='http://www.changeip.com'>http://www.changeip.com</a>"))
+weburlchangip:depends("service_name", "changeip.com")
+
+weburldyndns = s:option( DummyValue,"weburldyndns", translate("register url"),
+translate("<a target=\"_blank\" href='http://www.dyndns.org'>http://www.dyndns.org</a>"))
+weburldyndns:depends("service_name", "dyndns.org")
+
+
+weburlzoneedit = s:option( DummyValue,"weburlzoneedit", translate("register url"),
+translate("<a target=\"_blank\" href='http://www.zoneedit.com'>http://www.zoneedit.comg</a>"))
+weburlzoneedit:depends("service_name", "zoneedit.com")
+
+weburlfree_editdns = s:option( DummyValue,"weburlfree_editdns", translate("register url"),
+translate("<a target=\"_blank\" href='http://free.editdns.net'>http://free.editdns.net</a>"))
+weburlfree_editdns:depends("service_name", "free.editdns.net")
+
+
+weburlno_ip = s:option( DummyValue,"weburlno_ip", translate("register url"),
+translate("<a target=\"_blank\" href='http://www.no-ip.com'>http://www.no-ip.com</a>"))
+weburlno_ip:depends("service_name", "no-ip.com")
+weburlno_ip:depends("service_name", "noip.com")
+
+
+weburlfreedns_afraid = s:option( DummyValue,"weburlfreedns_afraid", translate("register url"),
+translate("<a target=\"_blank\" href='http://freedns.afraid.org'>http://freedns.afraid.org</a>"))
+weburlfreedns_afraid:depends("service_name", "freedns.afraid.org")
+
+weburlhe_net = s:option( DummyValue,"weburlhe_net", translate("register url"),
+translate("<a target=\"_blank\" href='http://dns.he.net'>http://dns.he.net</a>"))
+weburlhe_net:depends("service_name", "he.net")
+
+weburldnsmax = s:option( DummyValue,"weburldnsmax", translate("register url"),
+translate("<a target=\"_blank\" href='http://www.dnsmax.com'>http://www.dnsmax.com</a>"))
+weburldnsmax:depends("service_name", "dnsmax.com")
+
+weburlthatip = s:option( DummyValue,"weburlthatip", translate("register url"),
+translate("<a target=\"_blank\" href='http://www.thatip.com'>http://www.thatip.com</a>"))
+weburlthatip:depends("service_name", "thatip.com")
+
+
+weburlnamecheap = s:option( DummyValue,"weburlnamecheap", translate("register url"),
+translate("<a target=\"_blank\" href='http://www.namecheap.com'>http://www.namecheap.com</a>"))
+weburlnamecheap:depends("service_name", "namecheap.com")
+
+
 neiwang = s:option(Flag, "neiwang", translate("Intranet"),translate("Secondary route setting"))
 neiwang:depends("service_name", "3322.org")
 neiwang:depends("service_name", "dyndns.org")
