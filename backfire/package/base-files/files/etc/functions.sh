728400b5aa34e987d3f75aef30ecb00a217606e6
zjhzzyf <zjhzzyf@74e81aba-36f0-4891-8cfb-1815f9025a0c>
2012-02-08 05:17:30 +0000

    base-file 添加service_start  stop等函数

--- backfire/package/base-files/files/etc/functions.sh
+++ backfire/package/base-files/files/etc/functions.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (C) 2006 OpenWrt.org
+# Copyright (C) 2006-2011 OpenWrt.org
 # Copyright (C) 2006 Fokus Fraunhofer <carsten.tittel@fokus.fraunhofer.de>
 
 
@@ -275,7 +275,117 @@ uci_apply_defaults() {
 	uci commit
 }
 
+#
+# service: simple wrapper around start-stop-daemon
+#
+# Usage: service ACTION EXEC ARGS...
+#
+# Action:
+#   -C   check if EXEC is alive
+#   -S   start EXEC, passing it ARGS as its arguments
+#   -K   kill EXEC, sending it a TERM signal if not specified otherwise
+#
+# Environment variables exposed:
+#   SERVICE_DAEMONIZE    run EXEC in background
+#   SERVICE_WRITE_PID    create a pid-file and use it for matching
+#   SERVICE_MATCH_EXEC   use EXEC command-line for matching (default)
+#   SERVICE_MATCH_NAME   use EXEC process name for matching
+#   SERVICE_USE_PID      assume EXEC create its own pid-file and use it for matching
+#   SERVICE_NAME         process name to use (default to EXEC file part)
+#   SERVICE_PID_FILE     pid file to use (default to /var/run/$SERVICE_NAME.pid)
+#   SERVICE_SIG          signal to send when using -K
+#   SERVICE_SIG_RELOAD   default signal used when reloading
+#   SERVICE_SIG_STOP     default signal used when stopping
+#   SERVICE_STOP_TIME    time to wait for a process to stop gracefully before killing it
+#   SERVICE_UID          user EXEC should be run as
+#   SERVICE_GID          group EXEC should be run as
+#
+#   SERVICE_DEBUG        don't do anything, but show what would be done
+#   SERVICE_QUIET        don't print anything
+#
+
+SERVICE_QUIET=1
+SERVICE_SIG_RELOAD="HUP"
+SERVICE_SIG_STOP="TERM"
+SERVICE_STOP_TIME=5
+SERVICE_MATCH_EXEC=1
+
+service() {
+	local ssd
+	local exec
+	local name
+	local start
+	ssd="${SERVICE_DEBUG:+echo }start-stop-daemon${SERVICE_QUIET:+ -q}"
+	case "$1" in
+	  -C)
+		ssd="$ssd -K -t"
+		;;
+	  -S)
+		ssd="$ssd -S${SERVICE_DAEMONIZE:+ -b}${SERVICE_WRITE_PID:+ -m}"
+		start=1
+		;;
+	  -K)
+		ssd="$ssd -K${SERVICE_SIG:+ -s $SERVICE_SIG}"
+		;;
+	  *)
+		echo "service: unknown ACTION '$1'" 1>&2
+		return 1
+	esac
+	shift
+	exec="$1"
+	[ -n "$exec" ] || {
+		echo "service: missing argument" 1>&2
+		return 1
+	}
+	[ -x "$exec" ] || {
+		echo "service: file '$exec' is not executable" 1>&2
+		return 1
+	}
+	name="${SERVICE_NAME:-${exec##*/}}"
+	[ -z "$SERVICE_USE_PID$SERVICE_WRITE_PID$SERVICE_PID_FILE" ] \
+		|| ssd="$ssd -p ${SERVICE_PID_FILE:-/var/run/$name.pid}"
+	[ -z "$SERVICE_MATCH_NAME" ] || ssd="$ssd -n $name"
+	ssd="$ssd${SERVICE_UID:+ -c $SERVICE_UID${SERVICE_GID:+:$SERVICE_GID}}"
+	[ -z "$SERVICE_MATCH_EXEC$start" ] || ssd="$ssd -x $exec"
+	shift
+	$ssd${1:+ -- "$@"}
+}
+
+service_check() {
+	service -C "$@"
+}
+
+service_signal() {
+	SERVICE_SIG="${SERVICE_SIG:-USR1}" service -K "$@"
+}
+
+service_start() {
+	service -S "$@"
+}
+
+service_stop() {
+	local try
+	SERVICE_SIG="${SERVICE_SIG:-$SERVICE_SIG_STOP}" service -K "$@" || return 1
+	while [ $((try++)) -lt $SERVICE_STOP_TIME ]; do
+		service -C "$@" || return 0
+		sleep 1
+	done
+	SERVICE_SIG="KILL" service -K "$@"
+	sleep 1
+	! service -C "$@"
+}
+
+service_reload() {
+	SERVICE_SIG="${SERVICE_SIG:-$SERVICE_SIG_RELOAD}" service -K "$@"
+}
+
 service_kill() {
+	cat 1>&2 << __END_OF_WARNING__
+#
+# WARNING: the 'service_kill' function is now deprecated and might be 
+# removed soon. Consider using the other new service_* wrappers instead.
+#
+__END_OF_WARNING__
 	local name="${1}"
 	local pid="${2:-$(pidof "$name")}"
 	local grace="${3:-5}"
@@ -293,19 +403,60 @@ service_kill() {
 }
 
 
+group_add() {
+	local name="$1"
+	local gid="$2"
+	local rc
+	[ -f "${IPKG_INSTROOT}/etc/group" ] || return 1
+	[ -n "$IPKG_INSTROOT" ] || lock /var/lock/group
+	echo "${name}:x:${gid}:" >> ${IPKG_INSTROOT}/etc/group
+	rc=$?
+	[ -n "$IPKG_INSTROOT" ] || lock -u /var/lock/group
+	return $rc
+}
+
+group_exists() {
+	grep -qs "^${1}:" ${IPKG_INSTROOT}/etc/group
+}
+
+user_add() {
+	local name="${1}"
+	local uid="${2}"
+	local gid="${3:-$2}"
+	local desc="${4:-$1}"
+	local home="${5:-/var/run/$1}"
+	local shell="${6:-/bin/false}"
+	local rc
+	[ -f "${IPKG_INSTROOT}/etc/passwd" ] || return 1
+	[ -n "$IPKG_INSTROOT" ] || lock /var/lock/passwd
+	echo "${name}:x:${uid}:${gid}:${desc}:${home}:${shell}" >> ${IPKG_INSTROOT}/etc/passwd
+	rc=$?
+	[ -n "$IPKG_INSTROOT" ] || lock -u /var/lock/passwd
+	return $rc
+}
+
+user_exists() {
+	grep -qs "^${1}:" ${IPKG_INSTROOT}/etc/passwd
+}
+
+
 pi_include() {
 	if [ -f "/tmp/overlay/$1" ]; then
 		. "/tmp/overlay/$1"
 	elif [ -f "$1" ]; then
 		. "$1"
 	elif [ -d "/tmp/overlay/$1" ]; then
-		for src_script in /tmp/overlay/$1/*.sh; do
-			. "$src_script"
-		done
+		if [ -n "$(ls /tmp/overlay/$1/*.sh 2>/dev/null)" ]; then
+			for src_script in /tmp/overlay/$1/*.sh; do
+				. "$src_script"
+			done
+		fi
 	elif [ -d "$1" ]; then
-		for src_script in $1/*.sh; do
-			. "$src_script"
-		done
+		if [ -n "$(ls $1/*.sh 2>/dev/null)" ]; then
+			for src_script in $1/*.sh; do
+				. "$src_script"
+			done
+		fi
 	else
 		echo "WARNING: $1 not found"
 		return 1
