87a5621f1cc417577ff1745c0bc11ba7673f63be
lintel <lintel@74e81aba-36f0-4891-8cfb-1815f9025a0c>
2012-05-08 21:15:04 +0000

    Luci: *webcam模块移动到服务选项。

--- luci/trunk/luci/luci/applications/luci-webcam/luasrc/controller/webcam.lua
+++ luci/trunk/luci/luci/applications/luci-webcam/luasrc/controller/webcam.lua
@@ -7,7 +7,7 @@ function index()
 	if nixio.fs.access("/etc/config/mjpg-streamer") then
 	local page
 
-	page = entry({"admin", "externaldevice", "webcam"}, cbi("webcam"), _("webcam"), 60)
+	page = entry({"admin", "services", "webcam"}, cbi("webcam"), _("webcam"), 60)
 	page.i18n = "webcam"
 	page.dependent = true
 	end
