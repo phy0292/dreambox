21f5d21f35b3ea75743383e643dc60d4fbbb55de
richard.nee.cn <richard.nee.cn@74e81aba-36f0-4891-8cfb-1815f9025a0c>
2012-12-25 13:41:23 +0000

    target/linux/goldfish: *1 默认添加qemu-goldfish; *2 修改run-emulator.sh脚本

--- branches/sun/target/linux/goldfish/image/run-emulator.sh
+++ branches/sun/target/linux/goldfish/image/run-emulator.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+
+BASE=$(dirname $0)
 PREFIX=openwrt-goldfish-
-touch ${PREFIX}data.bin
-./goldfish-qemu/emulator -skindir ./goldfish-qemu/skins -skin HVGA -sysdir . -ramdisk ${PREFIX}ramdisk.bin -kernel ${PREFIX}kernel.bin -system ${PREFIX}system.bin -data ${PREFIX}data.bin -show-kernel -partition-size 100 -qemu -append "console=ttyS0 ubi.mtd=0 root=/dev/mtdblock3 rootfstype=jffs2 bootdelay=1 init=/etc/preinit" -net nic,vlan=0,model=smc91c111 -net user,vlan=0,hostname=OpenWrt
+QEMU_NAME=qemu-goldfish
+
+echo "Starting in $BASE/ DIR"
+
+touch $BASE/${PREFIX}data.bin
+
+$BASE/$QEMU_NAME/emulator \
+	-skindir $BASE/$QEMU_NAME/skins -skin HVGA \
+	-sysdir $BASE \
+	-ramdisk $BASE/${PREFIX}ramdisk.bin \
+	-kernel $BASE/${PREFIX}kernel.bin \
+	-system $BASE/${PREFIX}system.bin \
+	-data $BASE/${PREFIX}data.bin \
+	-show-kernel \
+	-partition-size 100 \
+	-qemu -append "console=ttyS0 ubi.mtd=0 root=/dev/mtdblock3 rootfstype=jffs2 bootdelay=1 init=/etc/preinit" \
+	-net nic,vlan=0,model=smc91c111 \
+	-net user,vlan=0,hostname=OpenWrt
